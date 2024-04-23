957585ed80b32c34a84c5141154129dc3d70766d
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-04-22 23:24:13 -0300

    v5

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "My ArgoCD studies app v4")
+		fmt.Fprintf(w, "My ArgoCD studies app v5")
 	})
 
 	http.ListenAndServe(":8080", nil)
